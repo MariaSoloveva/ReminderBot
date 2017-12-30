9dce6837773eb9deee4c3e211f09f19b0e757324
MariaSoloveva <32188528+MariaSoloveva@users.noreply.github.com>
2017-12-30 23:51:35 +0300

    Update api.cpp

--- api.cpp
+++ api.cpp
@@ -11,24 +11,26 @@ std::string readToken(const std::string& filename)
     telegramTokenFile >> token;
     return token;
 }
+
 void loop(Bot& bot)
 {
-    struct tm* u;
-    const time_t timer = time(NULL);
-    u = localtime(&timer);
     while(true)
     {
-        for(auto &g : list)
+
+        time_t now_t;
+        time(&now_t);
+        for (auto &g : list)
         {
             for (auto &f : g.second)
             {
-                if ((u->tm_year == f.dTm->tm_year) && (u->tm_mon == f.dTm->tm_mon)
-                    && (u->tm_mday == f.dTm->tm_mday) && (u->tm_hour == f.dTm->tm_hour)
-                    && (u->tm_min == f.dTm->tm_min) && (abs(u->tm_sec - f.dTm->tm_sec) < 30))
+                if ((abs(now_t - f.dTm) < 30))
+                {
                     bot.getApi().sendMessage(g.first, f.TextOfPrompt);
+                   
+                }
             }
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
 }
 
